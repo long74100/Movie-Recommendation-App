bb782aab7d888c1adce749f4e36451c6dec2377d
Long <lin.lo@husky.neu.edu>
2018-02-21 18:16:16 -0500

    updated security configuration

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/configurations/WebMvcConfig.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/configurations/WebMvcConfig.java
@@ -3,6 +3,7 @@ package edu.northeastern.cs4500.configurations;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @Configuration
@@ -12,5 +13,5 @@ public class WebMvcConfig extends WebMvcConfigurerAdapter {
 	public BCryptPasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
-
+	
 }
