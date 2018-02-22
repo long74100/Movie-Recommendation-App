1e22d8a8ba579a0406c050eaf01ad6c3bd0875ba
Edward Wang <wang.ed@husky.neu.edu>
2018-02-22 01:38:54 -0500

    remove this commit

---
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/js/components/HomePage.js
@@ -0,0 +1,14 @@
+const React = require('react');
+
+
+class HomePage extends React.Component {
+ render() {
+   return (
+       <div>
+           <h1> Home Page </h1>
+       </div>
+   );
+ }
+}
+
+export default HomePage;
---
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/js/components/header/Register.js
@@ -0,0 +1,10 @@
+import { Component } from 'react';
+
+class Register extends Component {
+ render() {
+   return (
+     <a>Register Here</a>
+   );
+ }
+}
+export default Register;
