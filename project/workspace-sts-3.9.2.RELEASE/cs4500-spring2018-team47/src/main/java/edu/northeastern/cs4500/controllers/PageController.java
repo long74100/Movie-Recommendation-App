1f91e8f9fd626d17ecea204a4ae9fc83fd8c0edf
Edward Wang <wang.ed@husky.neu.edu>
2018-02-22 03:05:48 -0500

    added a navigation bar

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/PageController.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/PageController.java
@@ -5,12 +5,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
-//@Controller
-//public class PageController {
-//	@RequestMapping(value={"/"}, method = RequestMethod.GET)
-//	public ModelAndView login(){
-//		ModelAndView modelAndView = new ModelAndView();
-//		modelAndView.setViewName("index");
-//		return modelAndView;
-//	}
-//}
+@Controller
+public class PageController {
+	@RequestMapping(value={"/"}, method = RequestMethod.GET)
+	public ModelAndView login(){
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("index");
+		return modelAndView;
+	}
+}
