187637c94ff6015406e6d398a7d761dd4a7469a3
Alec Li <li.ale@husky.neu.edu>
2018-03-27 18:05:44 -0400

    fixed js and html for review

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/movie.js
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/movie.js
@@ -1,6 +1,20 @@
 function init() {
 	const movieName = document.querySelector(".movie-title").innerText;
 	const ratingButtons = document.querySelectorAll(".rating input");
+	const submitReviewButton = document.querySelector("#writeReview");
+	
+	submitReviewButton.onclick = function() {
+		var reviewBox = document.querySelector("#review-box").value;
+		var encodedReview = encodeURI(reviewBox);
+		var movie = document.querySelector("#imdbID").innerText;
+		var encodedMovie = encodeURI(movie);
+		var url = "/writeReview";
+		var param = "review=" + encodedReview + "&movieId=" + encodedMovie;
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(param);
+	}
 
 	for (button in ratingButtons) {
 		ratingButtons[button].onclick = function() {
