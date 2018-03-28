de89ade68c603d63013328d2d618e5dc0e611190
Edward Wang <wang.ed@husky.neu.edu>
2018-03-28 02:33:20 -0400

    TEAM47-46 allow search to go to profile template page

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/UserprofileController.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/UserprofileController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
@@ -41,6 +42,13 @@ public class UserprofileController {
 		User user = userService.findUserByEmail(auth.getName());
 		modelAndView.addObject("user", user);
 		modelAndView.setViewName("movielist");
+		return modelAndView;
+	}
+	
+	@RequestMapping(value="/profile/{username}", method = RequestMethod.GET)
+    public ModelAndView userProfile(@PathVariable String username) {
+		ModelAndView modelAndView = new ModelAndView();
+		modelAndView.setViewName("fragments/userProfile/profilePage");
 		return modelAndView;
 	}
 	
