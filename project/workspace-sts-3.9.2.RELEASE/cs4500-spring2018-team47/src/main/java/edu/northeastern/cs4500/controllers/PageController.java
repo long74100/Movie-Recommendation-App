9a8bd931053786dda5bb023b8ae14ae6289c5195
Edward Wang <wang.ed@husky.neu.edu>
2018-03-27 20:38:53 -0400

    TEAM47-43 update user information in all controllers

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/PageController.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/controllers/PageController.java
@@ -1,9 +1,15 @@
 package edu.northeastern.cs4500.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
+
+import edu.northeastern.cs4500.model.services.UserService;
+import edu.northeastern.cs4500.model.user.User;
 
 /**
  * Spring MVC Controller to handle mappings for default pages.
@@ -12,9 +18,16 @@ import org.springframework.web.servlet.ModelAndView;
  */
 @Controller
 public class PageController {
+	
+	@Autowired
+    private UserService userService;
+	
 	@RequestMapping(value={"/"}, method = RequestMethod.GET)
 	public ModelAndView login(){
 		ModelAndView modelAndView = new ModelAndView();
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		User user = userService.findUserByEmail(auth.getName());
+		modelAndView.addObject("user", user);
 		modelAndView.setViewName("index");
 		return modelAndView;
 	}
