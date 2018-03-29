befdadc61ba660da697abcba80e856a27c2c6dc8
Edward Wang <wang.ed@husky.neu.edu>
2018-03-29 01:53:31 -0400

    TEAM47-42 accept received friend request

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/friendsAndRequests.js
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/friendsAndRequests.js
@@ -1,18 +1,32 @@
 $(function() {
 
-    $('#friend-list-link').click(function(e) {
+	$('#friend-list-link').click(function(e) {
 		$("#friend-list").delay(100).fadeIn(100);
- 		$("#request-list").fadeOut(100);
+		$("#request-list").fadeOut(100);
 		$('#request-list-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
 	$('#request-list-link').click(function(e) {
 		$("#request-list").delay(100).fadeIn(100);
- 		$("#friend-list").fadeOut(100);
+		$("#friend-list").fadeOut(100);
 		$('#friend-list-link').removeClass('active');
 		$(this).addClass('active');
 		e.preventDefault();
 	});
+
+	$('#acceptRequest').click(
+			function() {
+				alert("hello!");
+				var senderID = document.querySelector("#senderID").innerText;
+				var encodedAcceptRequest = encodeURI(senderID);
+				var url = "/acceptRequest";
+				var param = "senderID=" + encodedAcceptRequest;
+				var xhr = new XMLHttpRequest();
+				xhr.open("POST", url, true);
+				xhr.setRequestHeader("Content-type",
+						"application/x-www-form-urlencoded");
+				xhr.send(param);
+			});
 
 });
