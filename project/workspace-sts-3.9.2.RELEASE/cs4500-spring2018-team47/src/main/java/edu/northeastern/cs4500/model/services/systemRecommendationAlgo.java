e2790ab096fa3b3f75f0dd7eaa8f569e205402c3
Edward Wang <wang.ed@husky.neu.edu>
2018-04-11 00:24:45 -0400

    TEAM47-53 begin build method for differ matrix

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/model/services/systemRecommendationAlgo.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/model/services/systemRecommendationAlgo.java
@@ -8,6 +8,32 @@ import edu.northeastern.cs4500.model.movie.Movie;
 import edu.northeastern.cs4500.model.user.User;
 
 public class systemRecommendationAlgo {
+	Map<User,Map<Movie, Double>> data;
+	Map<Movie,Map<Movie, Double>> diff;
+	Map<Movie,Map<Movie, Integer>> freq;
 	
+	public systemRecommendationAlgo(Map<Movie,Double> data) {
+		this.data = data;
+		buildDiffMatrix();
+	}
+	
+	public void buildDiffMatrix() {
+		diff = new HashMap<>();
+		freq = new HashMap<>();
+		
+		for (Map<Movie, Double> user : data.values()) {
+			
+			for (Entry<Movie, Double> e : user.entrySet()) {
+				if (!diff.containsKey(e.getKey())) {
+					diff.put(e.getKey(), new HashMap<Movie, Double>());
+					freq.put(e.getKey(), new HashMap<Movie, Integer>());
+				}
+				
+				
+			}
+			
+		}
+		
+	}
 
 }
