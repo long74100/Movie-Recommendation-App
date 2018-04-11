2c4868227760f0c33184a9914796c20b654ee64d
Ruairi Maclennan-Smillie <rms@ccs.neu.edu>
2018-04-11 14:27:24 -0400

    TEAM47-34 Added movies in theaters

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/index.js
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/index.js
@@ -1,12 +1,33 @@
 $(document).ready(function(){
-			$('.your-class').slick({
+			$('.movie-banner').slick({
 				arrows: true,
 				dots: true,
-				slidesToShow: 5,
+				slidesToShow: 8,
 				slidesToScroll: 1,
 				autoplay: true,
 				autoplaySpeed: 2000,
-				
+				responsive: [
+					{
+						breakpoint: 2500,
+						settings: {
+							slidesToShow: 5
+						}
+					}
+					]
 			});
+			$('.movies-in-theaters-info').slick({
+				  slidesToShow: 1,
+				  slidesToScroll: 1,
+				  arrows: false,
+				  fade: true,
+				  asNavFor: '.movies-in-theaters-nav'
+				});
+			$('.movies-in-theaters-nav').slick({
+				slidesToShow: 3,
+				slidesToScroll: 1,
+				asNavFor: '.movies-in-theaters-info',
+				centerMode: true,
+				focusOnSelect: true
+				});
 			$('#body').show();
 });
