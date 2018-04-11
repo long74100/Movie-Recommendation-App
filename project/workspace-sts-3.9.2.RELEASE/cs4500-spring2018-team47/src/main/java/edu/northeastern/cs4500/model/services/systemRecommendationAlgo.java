556d1bc6985049386392f9bcb2f6dcda41480965
Edward Wang <wang.ed@husky.neu.edu>
2018-04-11 00:44:07 -0400

    TEAM47-53 finish writing buildDiffMatrix

--- project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/model/services/systemRecommendationAlgo.java
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/model/services/systemRecommendationAlgo.java
@@ -12,7 +12,7 @@ public class systemRecommendationAlgo {
 	Map<Movie,Map<Movie, Double>> diff;
 	Map<Movie,Map<Movie, Integer>> freq;
 	
-	public systemRecommendationAlgo(Map<Movie,Double> data) {
+	public systemRecommendationAlgo(Map<User,Map<Movie,Double>> data) {
 		this.data = data;
 		buildDiffMatrix();
 	}
@@ -28,12 +28,30 @@ public class systemRecommendationAlgo {
 					diff.put(e.getKey(), new HashMap<Movie, Double>());
 					freq.put(e.getKey(), new HashMap<Movie, Integer>());
 				}
-				
-				
+				for (Entry<Movie, Double> e2 : user.entrySet()) {
+					int oldCount = 0;
+					if (freq.get(e.getKey()).containsKey(e2.getKey())) {
+						oldCount = freq.get(e.getKey()).get(e2.getKey()).intValue();
+					}
+					double oldDiff = 0.0;
+					if (diff.get(e.getKey()).containsKey(e2.getKey())) {
+						oldDiff = diff.get(e.getKey()).get(e2.getKey()).doubleValue();
+					}
+					double observedDiff = e.getValue() - e2.getValue();
+					freq.get(e.getKey()).put(e2.getKey(), oldCount+1);
+					diff.get(e.getKey()).put(e2.getKey(), oldDiff + observedDiff);
+				}
 			}
 			
 		}
 		
+		for (Movie x : diff.keySet()) {
+			for (Movie y :diff.get(x).keySet()) {
+				double oldValue = diff.get(x).get(y).doubleValue();
+				int count = freq.get(x).get(y).intValue();
+				diff.get(x).put(y, oldValue / count);
+			}
+		}
 	}
 
 }
