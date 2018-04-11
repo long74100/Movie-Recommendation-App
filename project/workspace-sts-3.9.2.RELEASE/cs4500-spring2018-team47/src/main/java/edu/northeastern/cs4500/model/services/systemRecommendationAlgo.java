f0e614e39a1a0d90b03802c93f9430c6ddeb15f1
Edward Wang <wang.ed@husky.neu.edu>
2018-04-11 00:23:05 -0400

    TEAM47-53 create systemRec class

---
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/java/edu/northeastern/cs4500/model/services/systemRecommendationAlgo.java
@@ -0,0 +1,13 @@
+package edu.northeastern.cs4500.model.services;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+
+import edu.northeastern.cs4500.model.movie.Movie;
+import edu.northeastern.cs4500.model.user.User;
+
+public class systemRecommendationAlgo {
+	
+
+}
