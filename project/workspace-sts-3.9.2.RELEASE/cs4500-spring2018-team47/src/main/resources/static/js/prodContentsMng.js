17ccdc4cc39b829134d5b676a8507dad8c9a3861
lgj818 <lgj818@sina.cn>
2018-04-15 15:34:46 -0400

    TEAM47-54 Js file for delete/recall prod is not recording and uploaded

---
+++ project/workspace-sts-3.9.2.RELEASE/cs4500-spring2018-team47/src/main/resources/static/js/prodContentsMng.js
@@ -0,0 +1,59 @@
+function init() {
+	
+	
+}
+
+function deleteProd(senderId, receiverId, movieId, yourId, thisButton) {
+	var role = "";
+	if(senderId == yourId) {
+		role = "sender";
+	}
+	if(receiverId == yourId) {
+		role = "receiver";
+	}
+	
+	var encodedSenderId = encodeURI(senderId);
+	var encodedReceiverId = encodeURI(receiverId);
+	var encodedMovieId = encodeURI(movieId);
+	var encodedTarget = encodeURI(role);
+	var param = "senderId=" + encodedSenderId + "&receiverId=" + encodedReceiverId + "&prodMovie=" + encodedMovieId + "&side=" + encodedTarget;
+	var url = "/deleteProd"
+	var xhr = new XMLHttpRequest();
+	xhr.open("POST", url, true);
+	xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+	xhr.send(param);
+	setTimeout(function() {
+		location.reload(true);
+		}, 
+		1000);
+}
+
+
+function recallProd(senderId, receiverId, movieId, prodTime, thisButton) {
+	var prodDate = Date.parse(prodTime);
+	var recallTryDate = Date.now();
+	var difference = Math.abs(recallTryDate - prodDate);
+	/** Math.ceil(timeDiff / (1000 * 3600 * 24));*/
+	var differenceInMins = Math.ceil(difference / (1000 * 60));
+	if(differenceInMins > 10) {
+		alert("Sorry, you can't recall after 10 minutes.");
+	}
+	else {
+		var encodedSenderId = encodeURI(senderId);
+		var encodedReceiverId = encodeURI(receiverId);
+		var encodedMovieId = encodeURI(movieId);
+		var param = "senderId=" + encodedSenderId + "&receiverId=" + encodedReceiverId + "&prodMovie=" + encodedMovieId;
+		var url = "/recallProd"
+		var xhr = new XMLHttpRequest();
+		xhr.open("POST", url, true);
+		xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhr.send(param);
+		setTimeout(function() {
+			location.reload(true);
+			}, 
+			150);
+	}
+}
+
+
+document.addEventListener("DOMContentLoaded", init);
